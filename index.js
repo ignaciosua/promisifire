3e059b9077e8402c7e0a96d020d2d87da57a3197
ignaciosua.h <ignaciosua.h@gmail.com>
2021-01-03 16:22:27 -0600

    Updating validation parameters

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const promisifire = async (config) => {
     };
     if(!config.instance) return reject("promisifire: Missing instance of the function to be executed")
     if(!config.method) return reject("promisifire: Missing the method to be executed")
-    if(config.parameters.indexOf("success") < 0) return reject("promisifire: Missing success parameter")
-    if(config.parameters.indexOf("error") < 0) return reject("promisifire: Missing error parameter")
+    if(!config.parameters && config.parameters.indexOf("success") < 0) return reject("promisifire: Missing success parameter")
+    if(!config.parameters && config.parameters.indexOf("error") < 0) return reject("promisifire: Missing error parameter")
     config.parameters[config.parameters.indexOf('success')] = onSuccess;
     config.parameters[config.parameters.indexOf('error')] = onError;
     config.method.apply(config.instance,config.parameters);
